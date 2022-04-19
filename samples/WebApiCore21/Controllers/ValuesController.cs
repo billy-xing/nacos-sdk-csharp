59c1e3e27b4de5e6d2b99b85cf33ca90b7348050
billyxing <billyxing@ciicsh.com>
2022-04-19 11:22:47 +0800

    add samples for netcore 2.1

---
+++ samples/WebApiCore21/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿namespace WebApiCore21.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ValuesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var str = _config.GetValue("XXX", "5");
+            return str;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ samples/WebApiCore21/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace WebApiCore21
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Nacos.V2.DependencyInjection;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+
+            /*
+            var builder = CreateWebHostBuilder(args);
+
+            builder.Configuration
+                .AddEnvironmentVariables();
+
+
+            builder.Services.AddNacosV2Config(builder.Configuration.GetSection("NacosConfig"));
+
+            builder.Configuration.AddNacosV2Configuration(builder.Configuration.GetSection("NacosConfig"));
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+            app.MapControllers();
+
+            app.Run();
+            */
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddNacosV2Configuration(builder.Build().GetSection("NacosConfig"));
+                })
+                .UseStartup<Startup>();
+    }
+}
